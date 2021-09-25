131e65891d58452dca755dd098fdb0b35fb22f0c
Li <liqiang@skyon.com.cn>
2021-09-25 20:10:26 +0800

    工作台-新增预警任务列表Vo

---
+++ src/main/java/com/skyon/project/system/domain/vo/WarningTaskListVo.java
@@ -0,0 +1,76 @@
+package com.skyon.project.system.domain.vo;
+
+import com.skyon.framework.web.domain.BaseEntity;
+
+public class WarningTaskListVo extends BaseEntity {
+
+    private String taskHandler;
+
+    private String taskNo;
+
+    private String warningObjectName;
+
+    private String adBra;
+
+    private String adSubBra;
+
+    private String taskType;
+
+    private String taskStatus;
+
+    public String getTaskHandler() {
+        return taskHandler;
+    }
+
+    public void setTaskHandler(String taskHandler) {
+        this.taskHandler = taskHandler;
+    }
+
+    public String getTaskNo() {
+        return taskNo;
+    }
+
+    public void setTaskNo(String taskNo) {
+        this.taskNo = taskNo;
+    }
+
+    public String getWarningObjectName() {
+        return warningObjectName;
+    }
+
+    public void setWarningObjectName(String warningObjectName) {
+        this.warningObjectName = warningObjectName;
+    }
+
+    public String getAdBra() {
+        return adBra;
+    }
+
+    public void setAdBra(String adBra) {
+        this.adBra = adBra;
+    }
+
+    public String getAdSubBra() {
+        return adSubBra;
+    }
+
+    public void setAdSubBra(String adSubBra) {
+        this.adSubBra = adSubBra;
+    }
+
+    public String getTaskType() {
+        return taskType;
+    }
+
+    public void setTaskType(String taskType) {
+        this.taskType = taskType;
+    }
+
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+}
