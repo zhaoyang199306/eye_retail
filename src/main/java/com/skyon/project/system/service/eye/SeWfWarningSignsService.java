a7efeadd0f51ce4aa2df36fdf78629c0179bc13f
Li <liqiang@skyon.com.cn>
2021-10-12 11:19:01 +0800

    表单提交后，更改预警信号表内容

---
+++ src/main/java/com/skyon/project/system/service/eye/SeWfWarningSignsService.java
@@ -0,0 +1,7 @@
+package com.skyon.project.system.service.eye;
+
+import com.skyon.project.system.domain.eye.waringSings.SeWfWarningSigns;
+
+public interface SeWfWarningSignsService {
+    public int updateSeWfWarningSigns(SeWfWarningSigns seWfWarningSigns);
+}
---
+++ src/main/java/com/skyon/project/system/service/eye/impl/SeWfWarningSignsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.skyon.project.system.service.eye.impl;
+
+import com.skyon.project.system.domain.eye.waringSings.SeWfWarningSigns;
+import com.skyon.project.system.domain.eye.wf.SeWfTaskExecuteFeedback;
+import com.skyon.project.system.mapper.eye.wf.SeWfTaskExecuteFeedbackMapper;
+import com.skyon.project.system.mapper.eye.wf.SeWfWarningSignsMapper;
+import com.skyon.project.system.service.eye.SeWfTaskExecuteFeedbackService;
+import com.skyon.project.system.service.eye.SeWfWarningSignsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SeWfWarningSignsServiceImpl implements SeWfWarningSignsService {
+
+    private static final Logger logger = LoggerFactory.getLogger(SeWfWarningSignsServiceImpl.class);
+
+    @Autowired
+    private SeWfWarningSignsMapper seWfWarningSignsMapper;
+
+    public int updateSeWfWarningSigns(SeWfWarningSigns seWfWarningSigns){
+        return seWfWarningSignsMapper.updateSeWfWarningSigns(seWfWarningSigns);
+    }
+
+}
