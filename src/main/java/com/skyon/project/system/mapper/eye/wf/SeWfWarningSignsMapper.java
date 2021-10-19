948add46add35403048ede465ec83bde35281a87
1500201551 <1500201551@qq.com>
2021-10-19 09:38:50 +0800

    信号下发
    Signed-off-by: 1500201551 <1500201551@qq.com>

--- src/main/java/com/skyon/project/system/mapper/eye/wf/SeWfWarningSignsMapper.java
+++ src/main/java/com/skyon/project/system/mapper/eye/wf/SeWfWarningSignsMapper.java
@@ -1,9 +1,12 @@
 package com.skyon.project.system.mapper.eye.wf;
 
 import com.skyon.project.system.domain.eye.waringSings.SeWfWarningSigns;
+import com.skyon.project.system.domain.vo.SignDownListVo;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface SeWfWarningSignsMapper {
     public int updateSeWfWarningSigns(SeWfWarningSigns seWfWarningSigns);
 
@@ -13,5 +16,10 @@ public interface SeWfWarningSignsMapper {
      * @return
      */
     public int updateSingsSignalStatusById(List<String> id);
+
+    public List<SeWfWarningSigns> getSignalDownList(String listType);
+
+    public int updateDistributeStatus(SignDownListVo signDownListVo);
+
 
 }
