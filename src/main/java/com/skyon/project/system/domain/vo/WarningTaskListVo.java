535caddb87b18be974fcb6c09178b265a2ab654e
1500201551 <1500201551@qq.com>
2021-10-21 10:26:11 +0800

    预警列表查询入参实体类
    Signed-off-by: 1500201551 <1500201551@qq.com>

--- src/main/java/com/skyon/project/system/domain/vo/WarningTaskListVo.java
+++ src/main/java/com/skyon/project/system/domain/vo/WarningTaskListVo.java
@@ -3,27 +3,41 @@ package com.skyon.project.system.domain.vo;
 import java.util.List;
 
 import com.skyon.framework.web.domain.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.omg.CORBA.Request;
 
+@ApiModel(value = "预警页面请求信息",description ="预警页面请求信息查询")
 public class WarningTaskListVo extends BaseEntity {
 
+    @ApiModelProperty(value = "经办人", name = "taskHandler", required = false)
     private String taskHandler;
 
+    @ApiModelProperty(value = "查询任务", name = "taskNo", required = false)
     private String taskNo;
 
+    @ApiModelProperty(value = "带查询任务（后端使用）", name = "taskNoList", required = false)
     private List<String> taskNoList;
 
+    @ApiModelProperty(value = "模糊查询参数", name = "warningObjectLikeParam", required = false)
     private String  warningObjectLikeParam;
 
+    @ApiModelProperty(value = "预警对象名称", name = "warningObjectLikeParam", required = false)
     private String warningObjectName;
 
+    @ApiModelProperty(value = "预警对象编号", name = "warningObjectId", required = false)
     private String warningObjectId;
 
+    @ApiModelProperty(value = "管护人", name = "adBra", required = false)
     private String adBra;
 
+    @ApiModelProperty(value = "管护机构", name = "adSubBra", required = false)
     private String adSubBra;
 
+    @ApiModelProperty(value = "任务类型", name = "taskType", required = false)
     private String taskType;
 
+    @ApiModelProperty(value = "任务状态", name = "taskStatus", required = false)
     private String taskStatus;
 
     public String getWarningObjectLikeParam() {
